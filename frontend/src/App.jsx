6e401f22a2d846c2098fa245e44371b8afa43a89
AlexisCnockaert <uzzelekbeatspro@gmail.com>
2025-12-17 13:31:45 +0100

    Refactored css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
-import './styles/app.css';
+import './styles/base.css';
+import './styles/typography.css';
+import './styles/components.css';
+import './styles/icons.css';
 import HomeButton from './components/HomeButton';
 function App() {
     return (
