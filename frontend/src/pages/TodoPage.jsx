baf9d3452db57653da044fa1e8093d9daf598eef
AlexisCnockaert <uzzelekbeatspro@gmail.com>
2026-01-30 10:43:38 +0100

    bug

--- frontend/src/pages/TodoPage.jsx
+++ frontend/src/pages/TodoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import todoService from '../services/todoService';
 import ErrorMessage from '../components/layout/ErrorMessage';
